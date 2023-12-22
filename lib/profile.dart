5698fccaa27d2cb27ff56e00b976b1c32db8f644
M Tharun <tharun.m2022a@vitstudent.ac.in>
2023-12-22 11:57:00 +0530

    Update profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sit/Utilities/global.dart';
 
 void main() {
   runApp(ProfileApp());
@@ -26,16 +25,6 @@ class UserProfile extends StatefulWidget {
 }
 
 class _UserProfileState extends State<UserProfile> {
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController phoneController = TextEditingController();
-  final TextEditingController referredByController = TextEditingController();
-  final TextEditingController personNameController = TextEditingController();
-  final TextEditingController personMobileController = TextEditingController();
-  final TextEditingController professionalIntroController =
-      TextEditingController();
-  final TextEditingController websiteController = TextEditingController();
-
   final List<String> citiesInTamilNadu = [
     'Chennai',
     'Coimbatore',
@@ -44,17 +33,22 @@ class _UserProfileState extends State<UserProfile> {
     'Salem',
     'Tirunelveli',
   ];
-  String selectedCity = 'Chennai';
+  String selectedCity = 'Chennai'; // Assume you get this value from the backend
+
+  // Flag to indicate edit mode
+  bool isEditMode = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Update user data (you can replace this with your logic)
-          print('Updating user data...');
+          // Toggle edit mode
+          setState(() {
+            isEditMode = !isEditMode;
+          });
         },
-        child: Icon(Icons.done),
+        child: Icon(Icons.edit),
       ),
       body: SingleChildScrollView(
         child: Center(
@@ -73,39 +67,14 @@ class _UserProfileState extends State<UserProfile> {
                     ),
                   ),
                 ),
-                textFieldPrettier(context, nameController, 'Name'),
-                textFieldPrettier(context, emailController, 'Email'),
-                textFieldPrettier(context, phoneController, 'Phone'),
-                DropdownButtonFormField<String>(
-                  value: selectedCity,
-                  onChanged: (String? value) {
-                    setState(() {
-                      selectedCity = value!;
-                    });
-                  },
-                  items: citiesInTamilNadu.map((city) {
-                    return DropdownMenuItem<String>(
-                      value: city,
-                      child: Text(city),
-                    );
-                  }).toList(),
-                  decoration: InputDecoration(
-                    labelText: 'City',
-                    contentPadding: EdgeInsets.all(16.0),
-                  ),
-                ),
-                textFieldPrettier(
-                    context, referredByController, 'Referred By'),
-                textFieldPrettier(
-                    context, personNameController, 'Person Name'),
-                textFieldPrettier(
-                    context, personMobileController, 'Person Mobile No'),
-                textFieldPrettier(
-                    context,
-                    professionalIntroController,
-                    'Professional Intro'),
-                textFieldPrettier(
-                    context, websiteController, 'Websites / Social Media Handle Link'),
+                textPrettier(context, 'Name', 'John Doe', isEditMode),
+                textPrettier(context, 'Email', 'john.doe@example.com', isEditMode),
+                textPrettier(context, 'Phone', '+123 456 7890', isEditMode),
+                textPrettier(context, 'City', selectedCity, isEditMode),
+                textPrettier(context, 'Person Name', 'Jane Doe', isEditMode),
+                textPrettier(context, 'Person Mobile No', '+987 654 3210', isEditMode),
+                textPrettier(context, 'Professional Intro', 'Software Developer', isEditMode),
+                textPrettier(context, 'Websites / Social Media Handle Link', 'https://example.com', isEditMode),
               ],
             ),
           ),
@@ -113,5 +82,61 @@ class _UserProfileState extends State<UserProfile> {
       ),
     );
   }
-}
 
+  Widget textPrettier(BuildContext context, String labelText, String value, bool isEditable) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Theme.of(context).colorScheme.primary,
+            width: 1.0,
+          ),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 120,
+              padding: EdgeInsets.all(8),
+              child: Text(
+                labelText + ':',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: isEditable
+                  ? Container(
+                      padding: EdgeInsets.all(8),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                        controller: TextEditingController(text: value),
+                        onChanged: (newValue) {
+                          // Handle changes if needed
+                        },
+                      ),
+                    )
+                  : Container(
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        value,
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
